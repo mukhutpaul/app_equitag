0585535df5548f52485a49e21272ba5162107643
jp <mukhutjp@gmail.com>
2023-03-21 15:29:35 +0400

    modif

--- src/app/pages/darshboard/darshboard.component.ts
+++ src/app/pages/darshboard/darshboard.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { HomeService } from 'src/app/services/home.service';
 import { SnackbarService } from 'src/app/services/snackbar.service';
+import { GlobalConstants } from 'src/app/shared/global-constants';
 
 @Component({
   selector: 'app-darshboard',
@@ -8,10 +10,13 @@ import { SnackbarService } from 'src/app/services/snackbar.service';
   styleUrls: ['./darshboard.component.scss']
 })
 export class DarshboardComponent {
+  responseMessage:any;
+	data:any;
 
   constructor(
 		private ngxservice:NgxUiLoaderService,
-		private snackbarservice:SnackbarService
+		private snackbarservice:SnackbarService,
+    private dashboardService:HomeService
 		) {
 		
 		this.ngxservice.start();
@@ -21,9 +26,19 @@ export class DarshboardComponent {
 	}
 
   dashboardData(){
-  
+  this.dashboardService.gethome().subscribe((response:any)=>{
   this.ngxservice.stop();
- 
+  this.data=response?.detail;
+},(error:any)=>{
+  this.ngxservice.stop();
+  console.log(error);
+  if(error.error?.message){
+    this.responseMessage = error.error?.message;
+  }else{
+    this.responseMessage = GlobalConstants.genericError;
+  }
+     this.snackbarservice.openSnackBar(this.responseMessage,GlobalConstants.error);
+})
 }
 }
 
---
+++ src/app/services/home.service.ts
@@ -0,0 +1,64 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../environment/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  url = environment.apiUrl;
+
+  addGrade(data: any) {
+    return this.httpClient.post(this.url +
+      "/api/holder/grades/", data, {
+      headers: new HttpHeaders().set('Content-type',"application/json")
+    });
+  }
+
+  forgotPassword(data:any){
+     return this.httpClient.post(this.url+
+      "/api/holder/grades/",data,{
+        headers: new HttpHeaders().set('Content-type',"application/json")
+      })
+  }
+  login(data:any){
+    return this.httpClient.post(this.url+
+      "/api/login/",data,{
+        headers: new HttpHeaders().set('Content-type',"application/json")
+      })
+  }
+
+  checkToken(){
+    return this.httpClient.get(this.url + "/user/checkToken");
+  }
+
+  changePassword(data:any){
+    return this.httpClient.post(this.url+
+      "/user/changePassword",data,{
+        headers: new HttpHeaders().set('Content-Type',"application/json")
+      })
+  }
+
+  gethome(){
+    return this.httpClient.get(this.url+ "/api/home/");
+  }
+
+  update(id:any,data:any){
+    return this.httpClient.patch(this.url+"/api/holder/grades/"+id,data,{
+      headers: new HttpHeaders().set('Content-type',"application/json")
+
+    })
+  }
+
+  delete(id:any){
+    return this.httpClient.delete(this.url+"/api/holder/grades/"+id,{
+      headers: new HttpHeaders().set('Content-type',"application/json")
+
+    })
+  }
+
+
+}
